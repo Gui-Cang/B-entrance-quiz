0e53f0240e5365de5978527bd2f8ed09a5dbd74e
Gui-Cang <821192514@qq.com>
2020-10-27 22:21:42 +0800

    [dingke] feat: add CorsRegistry to fix CORS.

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -2,6 +2,9 @@ package com.thoughtworks.capability.gtb.entrancequiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GtbEntranceQuizApplication {
@@ -9,5 +12,12 @@ public class GtbEntranceQuizApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GtbEntranceQuizApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
